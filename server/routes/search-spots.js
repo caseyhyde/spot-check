c0947f7abe06d5cfad5203b7b41d533e9c27633a
Casey Hyde <hyde.casey@gmail.com>
2016-12-08 14:14:02 -0600

    Small changes

--- server/routes/search-spots.js
+++ server/routes/search-spots.js
@@ -20,18 +20,59 @@ router.get('/', function(req, res) {
     findZipAndKeywords();
   } else if (zip && !keywords) {
     findJustZip();
+  } else if (!zip && !keywords) {
+    findAll();
   }
 
-  
+  function findAll() {
+    console.log("Searching without Zipcode");
+    Spots.find( {},
+      function(err, spots) {
+      if(err) {
+        console.log("Query error searching database: ", err);
+        res.sendStatus(500);
+      } else {
+        console.log(spots);
+        if(spots == null) { //if no spots are found matching that zipcode:
+          console.log("There are no spots with the zipcode: ", searchTerms);
+          res.sendStatus(403);
+        } else {
+          console.log("Found these matches in the db: ", spots);
+          res.send(spots);
+        }
+      }
+    });
+  }
+
+  // function findOneLetter() {
+  //   console.log("find one letter");
+  //   Spots.find({spotName: new RegExp(keywords, 'i')}, function(err, spots) {
+  //     if(err) {
+  //       console.log("Querry error searching one letter: ", err);
+  //       res.sendStatus(500);
+  //     } else {
+  //       console.log("spots: ", spots);
+  //       if(spots === []) {
+  //         console.log("No spots have that letter anywhere...");
+  //         res.sendStatus(403);
+  //       } else {
+  //         console.log("Found these spots: ", spots);
+  //         res.send(spots);
+  //       }
+  //     }
+  //   })
+  // }
+
 
   function findZipAndKeywords() {
+    // var keywords = new RegExp(keywords, 'i');
     Spots.find( {$and: [{'address.zip': zip}, {$or: [
-      {spotName: keywords},
-      {'address.streetAddress': keywords},
-      {'address.city': keywords},
-      {'address.state': keywords},
-      {'details.notes': keywords},
-      {'destails.keywords': keywords}
+      {spotName: new RegExp(keywords, 'i')},
+      {'address.streetAddress': new RegExp(keywords, 'i')},
+      {'address.city': new RegExp(keywords, 'i')},
+      {'address.state': new RegExp(keywords, 'i')},
+      {'details.notes': new RegExp(keywords, 'i')},
+      {'destails.keywords': new RegExp(keywords, 'i')}
     ]}]},
       function(err, spots) {
       if(err) {
@@ -52,13 +93,14 @@ router.get('/', function(req, res) {
 
   function findJustKeywords() {
     console.log("Searching without Zipcode");
+    // var keywords = new RegExp(keywords, 'i');
     Spots.find( {$or: [
-      {spotName: keywords},
-      {'address.streetAddress': keywords},
-      {'address.city': keywords},
-      {'address.state': keywords},
-      {'details.notes': keywords},
-      {'destails.keywords': keywords}
+      {spotName: new RegExp(keywords, 'i')},
+      {'address.streetAddress': new RegExp(keywords, 'i')},
+      {'address.city': new RegExp(keywords, 'i')},
+      {'address.state': new RegExp(keywords, 'i')},
+      {'details.notes': new RegExp(keywords, 'i')},
+      {'destails.keywords': new RegExp(keywords, 'i')}
     ]},
       function(err, spots) {
       if(err) {
